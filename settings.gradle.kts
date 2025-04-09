e3e43acaab5f0a41640bb6de70baf0c37d8b9a7e
Maxim Keegan <keegan@tochka.com>
2025-04-09 17:40:12 +0500

    update upstream

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -51,6 +51,30 @@ include ("L29-threads")
 include ("L30-JMM")
 include ("L31-executors")
 
+include ("L32-concurrentCollections:ConcurrentCollections")
+include ("L32-concurrentCollections:QueueDemo")
+
+include ("L33-virtualThreads:base")
+include ("L33-virtualThreads:springBoot")
+
+include ("L34-multiprocess:processes-demo")
+include ("L34-multiprocess:sockets-demo")
+include ("L34-multiprocess:rmi-demo")
+include ("L34-multiprocess:grpc-demo")
+
+include ("L35-rabbitMQ:allServicesModels")
+include ("L35-rabbitMQ:approvalService")
+include ("L35-rabbitMQ:mainService")
+
+include ("L36-NIO")
+include ("L37-netty")
+
+include ("L38-webflux:source")
+include ("L38-webflux:processor")
+include ("L38-webflux:client")
+include ("L38-webflux-chat:client-service")
+include ("L38-webflux-chat:datastore-service")
+
 pluginManagement {
     val jgitver: String by settings
     val dependencyManagement: String by settings
